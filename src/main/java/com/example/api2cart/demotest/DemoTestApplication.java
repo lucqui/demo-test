d959125635ff1b5250c93298ccd9abd784fb6d94
Olexander <aspera180@gmail.com>
2018-07-18 13:15:09 +0300

    parse json

--- src/main/java/com/example/api2cart/demotest/DemoTestApplication.java
+++ src/main/java/com/example/api2cart/demotest/DemoTestApplication.java
@@ -3,12 +3,17 @@ package com.example.api2cart.demotest;
 import com.example.api2cart.demotest.service.MerchantService;
 import com.example.api2cart.demotest.service.TransactionService;
 import com.example.api2cart.demotest.service.UserService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -42,8 +47,15 @@ public class DemoTestApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        System.out.println(merchantService.getAll());
-        System.out.println(userService.getAll());
-        System.out.println(transactionService.getAll());
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                "https://api.api2cart.com/v1.0/order.list.json?api_key=79b8f23e2f9e13075a4dabb818b52632&store_key=2e41a1d35caaab4dc289e96fac57bbe2&start=0&count=4&params=force_all", String.class);
+
+        String gsonResponse = response.getBody();
+
+        System.out.println(gsonResponse);
+
+
     }
 }
